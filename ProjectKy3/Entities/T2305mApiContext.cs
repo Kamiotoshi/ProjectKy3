3230451ff4385fec1eac2793d3f033e9d079e54b
PC <PC@DESKTOP-R523CVF>
2024-08-20 10:52:52 +0700

    Add project files.

---
+++ ProjectKy3/Entities/T2305mApiContext.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectKy3.Entities;
+
+public partial class T2305mApiContext : DbContext
+{
+    public T2305mApiContext()
+    {
+    }
+
+    public T2305mApiContext(DbContextOptions<T2305mApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-R523CVF\\SQLEXPRESS;Initial Catalog=T2305M_API;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__categori__3213E83FF3E77D54");
+
+            entity.ToTable("categories");
+
+            entity.HasIndex(e => e.Name, "UQ__categori__72E12F1BB37D95D5")
+                .IsUnique()
+                .HasFilter("([name] IS NOT NULL)");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3213E83F8017090D");
+
+            entity.ToTable("products");
+
+            entity.HasIndex(e => e.CategoryId, "IX_products_category_id");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CategoryId).HasColumnName("category_id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(12, 4)")
+                .HasColumnName("price");
+            entity.Property(e => e.Thumbnail)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("thumbnail");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__products__catego__3A81B327");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3213E83FF39B7302");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Emaill, "UQ__users__A0DC84210F84A91F").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Emaill)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("emaill");
+            entity.Property(e => e.Fullname)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("fullname");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.Role)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasDefaultValue("user")
+                .HasColumnName("role");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ProjectKy3/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectKy3.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Fullname { get; set; } = null!;
+
+    public string Emaill { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public string Role { get; set; } = null!;
+}
